28da7e1f80e80b30b2bff71b9f8c4311ee934be6
aoisensi <me@aoisensi.info>
2023-05-07 16:19:19 +0900

    aaa

--- lib/hint.dart
+++ lib/hint.dart
@@ -8,7 +8,7 @@ class Hint extends StatelessWidget {
     return Column(
       children: const [
         Text(
-          'What\'s the food the Sun said?aaa',
+          'What\'s the food the Sun said?',
           style: TextStyle(
             color: Colors.white,
             fontSize: 32,
