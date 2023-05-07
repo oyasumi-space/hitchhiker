e670dab088be0f46eb26ad74b217f4e3fabf9d90
aoisensi <me@aoisensi.info>
2023-05-07 15:41:03 +0900

    add japanese hint

--- lib/hint.dart
+++ lib/hint.dart
@@ -5,14 +5,27 @@ class Hint extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'What\'s the food the Sun said?',
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: 32,
-        fontFamily: 'GameFont',
-        decoration: TextDecoration.none,
-      ),
+    return Column(
+      children: const [
+        Text(
+          'What\'s the food the Sun said?',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 32,
+            fontFamily: 'GameFont',
+            decoration: TextDecoration.none,
+          ),
+        ),
+        Text(
+          '太陽がしゃべった食べ物の名前は？',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontFamily: 'GameFontJP',
+            decoration: TextDecoration.none,
+          ),
+        ),
+      ],
     );
   }
 }
